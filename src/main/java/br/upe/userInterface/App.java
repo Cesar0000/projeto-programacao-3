c40e8b96e14fc66f99221ae2de57866a2ab22af5
Roberto Cesar <rc20022100@gmail.com>
2024-10-18 21:51:47 -0300

    Rename LoginApp.java
    The entry point file of the application has been renamed to App.java and moved out of the javafx directoy into its parent directory "src/main/java/br/upe/userInterface/".

--- src/main/java/br/upe/userInterface/App.java
+++ src/main/java/br/upe/userInterface/App.java
@@ -1,4 +1,4 @@
-package br.upe.userInterface.javafx;
+package br.upe.userInterface;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,18 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class LoginApp extends Application {
+import java.io.IOException;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws IOException {
         // Usando o caminho correto para o arquivo FXML
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginScreen.fxml"));
         primaryStage.setTitle("Login");
 
         primaryStage.setScene(new Scene(root, 920, 520));
         primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
