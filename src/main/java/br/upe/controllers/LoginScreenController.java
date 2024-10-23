3a6012f79cc45735aaec16ed4c964c367fcc0112
Roberto Cesar <rc20022100@gmail.com>
2024-10-23 18:24:46 -0300

    Update LoginScreenController.java

--- src/main/java/br/upe/controllers/LoginScreenController.java
+++ src/main/java/br/upe/controllers/LoginScreenController.java
@@ -1,4 +1,38 @@
 package br.upe.controllers;
 
+import br.upe.userInterface.AppContext;
+
+import java.io.IOException;
+
+import javafx.scene.control.Button;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
 public class LoginScreenController {
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private void handleLoginButtonClick() {
+    }
+
+    @FXML
+    private void handleRegisterButtonClick() throws IOException {
+        Parent registerScreen = FXMLLoader.load(getClass().getResource("/fxml/screens/RegisterScreen.fxml"));
+        Stage mainStage = AppContext.mainStage;
+        mainStage.getScene().setRoot(registerScreen);
+    }
 }
