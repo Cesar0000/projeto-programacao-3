84236a3f84c60b6bd90a2805a29c82479f0ff1ee
Winiciuscx <carloswinicius055@gmail.com>
2024-10-23 22:14:07 -0300

    Add EventScreenController.java

---
+++ src/main/java/br/upe/controllers/EventScreenController.java
@@ -0,0 +1,22 @@
+package br.upe.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class EventScreenController {
+
+    @FXML
+    private ListView<?> eventList;
+
+    @FXML
+    void Atualizar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Voltar(ActionEvent event) {
+
+    }
+
+}
