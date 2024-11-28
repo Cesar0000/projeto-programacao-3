7f6945d156fba2cfa95de856aa654bdd7967abdb
Roberto Cesar <rc20022100@gmail.com>
2024-11-27 21:58:56 -0300

    Add SubEvent.java
    The SubEvent jpa entity has been added to the br.upe.persistence.entities package.

---
+++ src/main/java/br/upe/persistence/entities/SubEvent.java
@@ -0,0 +1,47 @@
+package br.upe.persistence.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "SubEvents")
+public class SubEvent extends Event {
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(
+        name = "superEventId",
+        referencedColumnName = "id",
+        nullable = false,
+        foreignKey = @ForeignKey(name = "subEventToSuperEventFK")
+    )
+    private SuperEvent superEvent;
+
+    protected SubEvent() {}
+
+    public SubEvent(Account account, String name, SuperEvent superEvent) {
+        super(account, name);
+        this.superEvent = superEvent;
+    }
+
+    public SuperEvent getSuperEvent() {
+        return superEvent;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+
+        return obj instanceof SubEvent otherSubEvent
+            && id != null
+            && id.equals(otherSubEvent.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
