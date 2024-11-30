4b8ad80057c633e10c220abd9a9890c7c5edd3f0
Roberto Cesar <rc20022100@gmail.com>
2024-11-30 14:44:09 -0300

    Update Account.java

--- src/main/java/br/upe/persistence/entities/Account.java
+++ src/main/java/br/upe/persistence/entities/Account.java
@@ -1,12 +1,15 @@
 package br.upe.persistence.entities;
 
+// JPA imports
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Basic;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
 
+// Java imports
 import java.time.Instant;
 import java.util.UUID;
 
@@ -19,8 +22,12 @@ public class Account {
     public static final int NAME_MAX_LENGTH = 50;
 
     @Id
-    @GeneratedValue
-    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(
+        name = "id",
+        nullable = false,
+        updatable = false
+    )
     private UUID id;
 
     @Basic(optional = false)
@@ -28,6 +35,7 @@ public class Account {
         name = "username",
         nullable = false,
         unique = true,
+        updatable = false,
         length = USERNAME_MAX_LENGTH
     )
     private String username;
@@ -57,7 +65,11 @@ public class Account {
     private String name;
 
     @Basic(optional = false)
-    @Column(name = "createdAtTimestamp", nullable = false)
+    @Column(
+        name = "createdAtTimestamp",
+        nullable = false,
+        updatable = false
+    )
     private Instant createdAtTimestamp = Instant.now();
 
     protected Account() {}
